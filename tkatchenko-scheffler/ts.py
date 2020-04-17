fa70bca4cedad1cded75061b7f834fd73480a354
Miguel Caro <mcaroba@gmail.com>
2020-04-17 14:33:11 +0300

    Fixed ts.py

--- tkatchenko-scheffler/ts.py
+++ tkatchenko-scheffler/ts.py
@@ -16,13 +16,20 @@ except:
     from ase import Atoms
     atoms = Atoms(s22[system]["symbols"], s22[system]["positions"])
 
+# We have at least 4 Angstrom of vacuum in every direction;
+# along z we add 2 extra Angstrom because we will shif the
+# methane by that amount. This is better than adjusting the
+# amount of vacuum for each shift, since we prevent jumps in
+# the PES from taking place when the discratization changes
+atoms.center(vacuum = 4.)
+atoms.cell[2][2] += 2.
+
 # Now let's shift the position of the methane molecule relative
 # to the benzene
 for z_shift in np.arange(-1.,2.,0.1):
     atoms_shifted = atoms.copy()
     for i in range(12,17):
         atoms_shifted.positions[i] += [0, 0, z_shift]
-    atoms_shifted.center(vacuum = 4.)
     calc = GPAW(xc="PBE", txt="gpaw.out")
     atoms_shifted.set_calculator(calc)
     e = atoms_shifted.get_potential_energy()
